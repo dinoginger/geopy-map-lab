c038c522cb71cb669b969a8d4248f7c686cb2cbf
marco <marcothemars@gmail.com>
2022-02-07 08:43:40 +0200

    made an argparse

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ Marko Ruzak, APPS_2021, CS-1
 
 import argparse
 
+
 def argparser():
     parser = argparse.ArgumentParser(description='Process input parameters for generating'
-                                                 'geo map script.')
+                                                 ' geo map script.')
     parser.add_argument('latitude', metavar='Latitude', type=float,
                         help='latitude for generating geo map.')
     parser.add_argument('longitude', metavar='Longitude', type=float,
                         help='longitude for generating geo map.')
     parser.add_argument('year', metavar='Year', type=int,
                         help='year of publishing for movies, for generating geo map.')
-    parser.add_argument('path', metavar='Path to file', type=float,
+    parser.add_argument('path', metavar='Path', type=str,
                         help='Path to the film db file. '
                              'Contains coordinates and year of publishing')
+    return parser
 
-    args = parser.parse_args()
-    print(args.accumulate(args.integers))
 
 if __name__ == "__main__":
-    argparser()
+    input_params = argparser().parse_args()
